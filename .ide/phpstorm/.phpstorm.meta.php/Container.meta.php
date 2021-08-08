dfc1b81769929ed3274cb339599dae0f37cfc984
Simon Asika <asika32764@gmail.com>
2021-08-08 14:16:23 +0800

    4.0.0-beta1

--- .ide/phpstorm/.phpstorm.meta.php/Container.meta.php
+++ .ide/phpstorm/.phpstorm.meta.php/Container.meta.php
@@ -14,6 +14,15 @@ namespace PHPSTORM_META {
     use Windwalker\DI\Container;
 
     override(
+        Container::get(0),
+        map(
+            [
+                '' => '@',
+            ]
+        )
+    );
+
+    override(
         Container::createObject(0),
         map(
             [
@@ -33,6 +42,15 @@ namespace PHPSTORM_META {
 
     override(
         Container::newInstance(0),
+        map(
+            [
+                '' => '@',
+            ]
+        )
+    );
+
+    override(
+        Container::resolve(0),
         map(
             [
                 '' => '@',
